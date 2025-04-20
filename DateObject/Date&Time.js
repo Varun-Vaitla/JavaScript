7939d0860d8629b3ede029eb156cb78b24f1ba02
Varun Vaitla <varunvm996@gmail.com>
2025-04-20 20:26:11 +0530

    Add files via upload

---
+++ DateObject/Date&Time.js
@@ -0,0 +1,13 @@
+let ref=new Date();
+console.log(ref.getFullYear());//2025
+console.log(ref.getMonth());//jan starts from 0
+console.log(ref.getDate());
+console.log(ref.getDay());//sunday starts with 0
+console.log(ref.getHours());
+console.log(ref.getMinutes());
+console.log(ref.getSeconds());
+console.log(ref.getTime());//1745160838984
+console.log(ref.getMilliseconds());//349
+
+
+
